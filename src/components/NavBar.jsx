7180cbc6b013a60fc70f43fdcf93422856094ac7
charlesnanda <charlesnanda6@outlook.com>
2026-07-29 19:46:31 -0600

    commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,55 @@
+import {Command} from "lucide-react";
+
+
+const NavBar = () => {
+  return (
+      <header className="fixed cursor-pointer top-3.5 left-1/2
+      -translate-x-1/2 z-50 transition-all duration-300 rounded-full h-14
+      bg-[#1b1b1b] border border-white/10 scale--95 w-[90%] max-w-2xl">
+
+        <div className="mx-auto h-full px-6">
+
+    <nav className="centered-row justify-between h-full">
+        {/* Logo */}
+      <div className="centered-row gap-2 hover:-translate-y-1
+      duration-200 transition-all ease-out">
+
+        <Command className="w-5 h-5 text-blue-400"/>
+
+        <span className="font-blod text-base
+        clash-display">ForexPoint</span>
+
+      </div>
+        {/* Desktop Navigation */}
+
+      <div className="hidden md:centered-row gap-6">
+        {["Features", "Prices", "Testimonials"].map((item,index)=>(
+            <a key={index} href="nav_link" className="text-sm
+            text-zinc-400 hover:text-blue-200 hover-translate-y-1
+            ease-out transition-all duration-300">
+              {item}
+            </a>
+        ))}
+        {/* Button */}
+
+      <button className="clash-display text-base bg-gradient-to-r
+      from-blue-400 to-blue-600 px-4 py-2 rounded-full
+      cursor-point hover:-translate-y-0.5 duration-200
+      transition-all ease-out hover:shadow-xl
+      hover:shadow-blue-900">Start Trading</button>
+
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className="md:hidden glass p-1 rounded-md">
+        <img src="/menu.svg" alt="menu-icon" className="w-8"/>
+      </div>
+
+    </nav>
+
+        </div>
+
+  </header>
+  )}
+
+export default NavBar
